936e02694a31c11070d73ee78f7b365ff282a282
JorgeVS <jorge.villalobos@gorillalogic.com>
2019-02-12 22:40:54 -0600

    some more styling

--- src/modules/questionnaire/components/questionnaire-list/questionnaire-list.component.ts
+++ src/modules/questionnaire/components/questionnaire-list/questionnaire-list.component.ts
@@ -2,11 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { QuestionnaireService } from 'src/modules/questionnaire/services';
 import { IQuestionnaire } from 'src/modules/app/models';
 import { Observable } from 'rxjs';
+import { trigger, style, transition, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-questionnaire-list',
   templateUrl: './questionnaire-list.component.html',
-  styleUrls: ['./questionnaire-list.component.scss']
+  styleUrls: ['./questionnaire-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      transition(':enter', [
+        style({transform: 'translateY(-100px)'}),
+        animate(350)
+      ]),
+      transition(':leave', [
+        group([
+          animate('2.2s ease', style({
+            transform: 'translateY(-100px)'
+          })),
+          animate('0.5s 0.2s ease', style({
+            opacity: 0
+          }))
+        ])
+      ])
+    ])
+  ]
 })
 export class QuestionnaireListComponent implements OnInit {
 
@@ -14,8 +33,18 @@ export class QuestionnaireListComponent implements OnInit {
 
   constructor(private questionnaireService: QuestionnaireService) { }
 
+  showMessage: boolean = false;
+
   ngOnInit(): void {
     this.questionnaireService.loadCurrentQuestionnaires(1);
   }
+
+  isDisabled() : void{
+    this.showMessage = true;
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 2000);
+  }
+
 
 }
